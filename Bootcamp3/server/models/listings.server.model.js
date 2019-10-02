7c114e9e73934ae1a3905c29fb739d73c15f5e4d
Jake Haskell <jakeahaskell@gmail.com>
2019-10-02 17:39:36 -0400

    everything but tests working

--- Bootcamp3/server/models/listings.server.model.js
+++ Bootcamp3/server/models/listings.server.model.js
@@ -7,12 +7,35 @@ var mongoose = require('mongoose'),
 /* Create your schema */
 var listingSchema = new Schema({
   /* your code here from Bootcamp Assignment #2 - ListingSchema.js File*/
-    
+  code: { type: String, required: true, unique: true },
+  name: { type: String, required: true },
+  coordinates: {
+    latitude: Number,
+    longitude: Number
+  },
+  address: String,
+
+  created_at: Date,
+  updated_at: Date
 });
 
 /* create a 'pre' function that adds the updated_at and created_at if not already there property */
 listingSchema.pre('save', function(next) {
   /* your code here from Bootcamp Assignment #2 - ListingSchema.js File */
+  if (!this.code){
+    throw new Error("code undefined");
+  }
+  if (!this.name) {
+    throw new Error("name undefined");
+  }
+
+  var currentDate = new Date();
+
+  this.updated_at = currentDate;
+
+  if(!this.created_at){
+    this.created_at = currentDate;
+  }
 
   next();
 });
